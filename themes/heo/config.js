4cf7e8bbfb4799e76fe40315a3743195cc19b11b
yaj4609 <152792273+yaj4609@users.noreply.github.com>
2024-12-17 14:20:54 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -7,10 +7,10 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
+  /*HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
     { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  ],*/
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
